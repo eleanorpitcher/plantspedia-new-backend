7da3ef44959a69e929a103a4f5d5e8d3dcd7e8ac
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-04-05 12:19:36 +0200

    middleware and verify route done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -97,7 +97,8 @@ router.post('/login', (req,res,next)=>{
 //verify
 
 router.get('/verify', isAuthenticated, (req,res,next)=>{
-
+    console.log(`req.payload`, req.payload)
+    res.status(200).json(req.payload)
 })
 
 
